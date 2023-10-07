36f6a8f75a9273779435a1954b51da682a1c2cd2
Yossi Kreinin <Yossi.Kreinin@gmail.com>
2023-10-07 16:45:35 +0300

    making the cursor look like a pencil (transparent to not occlude the drawn lines)

--- pygame/tinymation.py
+++ pygame/tinymation.py
@@ -1,26 +1,61 @@
 import pygame
+pg = pygame
 
 screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
 
-BLUE = (0 , 0 , 255)
+PEN = (0 , 0 , 0)
+WIDTH = 5
+CURSOR_SIZE = 150
+
+screen.fill((255,255,255))
+pygame.display.flip()
 
 def drawCircle( screen, x, y ):
-  pygame.draw.circle( screen, BLUE, ( x, y ), 5 )
+  pygame.draw.circle( screen, PEN, ( x, y ), WIDTH/2 )
+
+def drawLine(screen, pos1, pos2):
+  pygame.draw.line( screen, PEN, pos1, pos2, WIDTH )
+
+def load_cursor(file):
+  surface = pg.image.load(file)
+  surface = pg.transform.scale(surface, (CURSOR_SIZE, CURSOR_SIZE))
+  for y in range(CURSOR_SIZE):
+      for x in range(CURSOR_SIZE):
+          r,g,b,a = surface.get_at((x,y))
+          surface.set_at((x,y), (r,g,b,min(a,192)))
+  return pg.cursors.Cursor((10,CURSOR_SIZE-7), surface)
+
+pencil_cursor = load_cursor('pencil.png')
+pg.mouse.set_cursor(pencil_cursor)
 
 isPressed = False
 escape = False
+prevDrawn = None
 while not escape: 
   for event in pygame.event.get():
+   try:
     if event.type == pygame.KEYDOWN and event.key == 27: # ESC pressed
         escape = True
     if event.type == pygame.MOUSEBUTTONDOWN:
       isPressed = True
     elif event.type == pygame.MOUSEBUTTONUP:
       isPressed = False
+      if prevDrawn:
+          drawCircle(screen, *prevDrawn)
+      pygame.display.flip()
+      prevDrawn = None
     elif event.type == pygame.MOUSEMOTION and isPressed == True:
       ( x, y ) = pygame.mouse.get_pos()       # returns the position of mouse cursor
-      drawCircle( screen, x, y )
+      if prevDrawn:
+          drawLine(screen, prevDrawn, (x,y))
+      else:
+          drawCircle( screen, x, y )
+      prevDrawn = (x,y)
       pygame.display.flip()
+   except:
+    print('INTERNAL ERROR (printing and continuing)')
+    import traceback
+    traceback.print_exc()
       
 pygame.display.quit()
 pygame.quit()
