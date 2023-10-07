903877bb4c738dc786bfd9abb323300e1d79de91
Yossi Kreinin <Yossi.Kreinin@gmail.com>
2023-10-07 22:04:11 +0300

    added erasers of 3 sizes (cursor TBD)

--- pygame/tinymation.py
+++ pygame/tinymation.py
@@ -3,18 +3,19 @@ pg = pygame
 
 screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
 
-PEN = (0 , 0 , 0)
+PEN = (20, 20, 20)
+BACKGROUND = (240, 235, 220)
 WIDTH = 5
 CURSOR_SIZE = 150
 
-screen.fill((255,255,255))
+screen.fill(BACKGROUND)
 pygame.display.flip()
 
-def drawCircle( screen, x, y ):
-  pygame.draw.circle( screen, PEN, ( x, y ), WIDTH/2 )
+def drawCircle( screen, x, y, color, width):
+  pygame.draw.circle( screen, color, ( x, y ), width/2 )
 
-def drawLine(screen, pos1, pos2):
-  pygame.draw.line( screen, PEN, pos1, pos2, WIDTH )
+def drawLine(screen, pos1, pos2, color, width):
+  pygame.draw.line( screen, color, pos1, pos2, width )
 
 def load_cursor(file):
   surface = pg.image.load(file)
@@ -29,8 +30,8 @@ pencil_cursor = load_cursor('pencil.png')
 pg.mouse.set_cursor(pencil_cursor)
 
 class HistoryItem:
-    def __init__(self, surface):
-        self.surface = surface
+    def __init__(self):
+        self.surface = screen.copy()
         self.minx = 10**9
         self.miny = 10**9
         self.maxx = -10**9
@@ -61,6 +62,29 @@ class HistoryItem:
         self.surface = affected
         self.optimized = True
 
+class PenTool:
+    def __init__(self, color=PEN, width=WIDTH):
+        self.prev_drawn = None
+        self.color = color
+        self.width = width
+
+    def on_mouse_down(self):
+        history.append(HistoryItem())
+
+    def on_mouse_up(self):
+        self.prev_drawn = None
+        if history:
+            history[-1].optimize()
+
+    def on_mouse_move(self, x, y):
+       if history:
+            history[-1].affected(x-self.width,y-self.width,x+self.width,y+self.width)
+       if self.prev_drawn:
+            drawLine(screen, self.prev_drawn, (x,y), self.color, self.width)
+       drawCircle( screen, x, y, self.color, self.width )
+       self.prev_drawn = (x,y)
+
+tool = PenTool()
 history = []
 isPressed = False
 escape = False
@@ -71,31 +95,35 @@ while not escape:
     if event.type == pygame.KEYDOWN:
         if event.key == 27: # ESC pressed
             escape = True
-        elif event.key == ord(' '):
+            
+        if isPressed:
+            continue # ignore keystrokes (except ESC) when a mouse tool is being used
+        
+        if event.key == ord(' '): # undo
             if history:
                 history[-1].undo()
                 history.pop()
+
+        # TODO: condition on "adult mode"
+        elif event.key in [ord('e'), ord('E')]:
+            tool = PenTool(BACKGROUND, WIDTH)
+        elif event.key in [ord('r'), ord('R')]:
+            tool = PenTool(BACKGROUND, WIDTH*5)
+        elif event.key in [ord('t'), ord('T')]:
+            tool = PenTool(BACKGROUND, WIDTH*20)
+        elif event.key in [ord('b'), ord('B')]:
+            tool = PenTool()
             
     if event.type == pygame.MOUSEBUTTONDOWN:
-      history.append(HistoryItem(screen.copy()))
       isPressed = True
+      tool.on_mouse_down()
     elif event.type == pygame.MOUSEBUTTONUP:
       isPressed = False
-      if prevDrawn:
-          drawCircle(screen, *prevDrawn)
-      if history:
-          history[-1].optimize()
+      tool.on_mouse_up()
       pygame.display.flip()
-      prevDrawn = None
     elif event.type == pygame.MOUSEMOTION and isPressed == True:
       ( x, y ) = pygame.mouse.get_pos()       # returns the position of mouse cursor
-      if history:
-          history[-1].affected(x-WIDTH,y-WIDTH,x+WIDTH,y+WIDTH)
-      if prevDrawn:
-          drawLine(screen, prevDrawn, (x,y))
-      else:
-          drawCircle( screen, x, y )
-      prevDrawn = (x,y)
+      tool.on_mouse_move(x,y)
       pygame.display.flip()
    except:
     print('INTERNAL ERROR (printing and continuing)')
