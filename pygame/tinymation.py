0b54193d6350e5b7fc75a44767173e024dedce3d
Yossi Kreinin <Yossi.Kreinin@gmail.com>
2023-10-07 18:24:44 +0300

    optimized undo (save/restore just the affected area)

--- pygame/tinymation.py
+++ pygame/tinymation.py
@@ -31,9 +31,35 @@ pg.mouse.set_cursor(pencil_cursor)
 class HistoryItem:
     def __init__(self, surface):
         self.surface = surface
+        self.minx = 10**9
+        self.miny = 10**9
+        self.maxx = -10**9
+        self.maxy = -10**9
+        self.optimized = False
     def undo(self):
-        screen.blit(self.surface, screen.get_rect())
+        if self.optimized:
+            screen.blit(self.surface, (self.minx, self.miny), (0, 0, self.maxx-self.minx+1, self.maxy-self.miny+1))
+        else:
+            screen.blit(self.surface, screen.get_rect())
         pygame.display.flip()
+    def affected(self,minx,miny,maxx,maxy):
+        self.minx = min(minx,self.minx)
+        self.maxx = max(maxx,self.maxx)
+        self.miny = min(miny,self.miny)
+        self.maxy = max(maxy,self.maxy)
+    def optimize(self):
+        if self.minx == 10**9:
+            return
+        left, bottom, right,top = screen.get_rect()
+        self.minx = max(self.minx, left)
+        self.maxx = min(self.maxx, right-1)
+        self.miny = max(self.miny, bottom)
+        self.maxy = min(self.maxy, top-1)
+        
+        affected = pg.Surface((self.maxx-self.minx+1, self.maxy-self.miny+1), screen.get_flags(), screen.get_bitsize(), screen.get_masks())
+        affected.blit(self.surface, (0,0), (self.minx, self.miny, self.maxx+1, self.maxy+1))
+        self.surface = affected
+        self.optimized = True
 
 history = []
 isPressed = False
@@ -57,10 +83,14 @@ while not escape:
       isPressed = False
       if prevDrawn:
           drawCircle(screen, *prevDrawn)
+      if history:
+          history[-1].optimize()
       pygame.display.flip()
       prevDrawn = None
     elif event.type == pygame.MOUSEMOTION and isPressed == True:
       ( x, y ) = pygame.mouse.get_pos()       # returns the position of mouse cursor
+      if history:
+          history[-1].affected(x-WIDTH,y-WIDTH,x+WIDTH,y+WIDTH)
       if prevDrawn:
           drawLine(screen, prevDrawn, (x,y))
       else:
